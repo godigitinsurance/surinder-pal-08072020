b0d65f6997fe659d977bd6bfabc7f4f656d6870b
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-07 10:25:01 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Surinder Pal");
+$("#policyNumber").text("ULN7500817");
+$("#startDate").text("08-Jul-2020");
+$("#endDate").text("07-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08BW4726");
+$("#make").text("HERO HONDA");
+$("#model").text("PASSION PLUS ");
+$("#vehicalSubType").text("CCR");
+$("#engineNo").text("HA10EFBHH00715");
+$("#chassis").text("MBLHA10EZBHH00939");
